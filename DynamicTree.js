3086d1d98f43eecb01bc0ce6c501b0fa0eb45063
msyoder <yoderm01@gmail.com>
2014-12-02 00:49:23 -0500

    added comments for goofy tree recreation

--- DynamicTree.js
+++ DynamicTree.js
@@ -26,7 +26,7 @@ function TreeCtrl($scope){
 	Tree with foilage	-82				135		13 		1.8 		2.2 		19 			31			16
 	Bronchial			-90 			90 		10 		3.0 		3.0 		33 			33 			10
 	One-Sided			-95				150 	12 		1.8 		0.002 		30 			0 			16
-	Goofy				-90				236		236 	1.2 		2.4 		48			85			12 // and set rightLengthFactor to equal the left
+	Goofy				-90				236		45 		1.2 		2.4 		48			85			12 // and set rightLengthFactor to equal the leftAlpha
 
 	*/
 
@@ -43,7 +43,8 @@ function TreeCtrl($scope){
 		$scope.leftWidthFactor = Math.pow(2, -1/$scope.treeVars.leftAlpha);
 		$scope.rightWidthFactor = Math.pow(2, -1/$scope.treeVars.rightAlpha);
 		$scope.leftLenghtFactor = Math.pow(2, -2/(3*$scope.treeVars.leftAlpha));
-		$scope.rightLengthFactor = Math.pow(2, -2/(3*$scope.treeVars.rightAlpha));
+		$scope.rightLengthFactor = Math.pow(2, -2/(3*$scope.treeVars.right12Alpha)); // For expected tree
+	//	$scope.rightLengthFactor = Math.pow(2, -2/(3*$scope.treeVars.leftAlpha)); // For goofy tree
 	}
 
 	function drawLine(x1, y1, x2, y2, width){
